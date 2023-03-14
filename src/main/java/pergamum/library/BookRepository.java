6bbd0994dbd52b1d4577d9876462a838b2057b14
cebucmihai <mihaicebuc36@gmail.com>
2023-03-14 14:31:46 +0200

    Backend Ness Project v1

--- src/main/java/pergamum/library/BookRepository.java
+++ src/main/java/pergamum/library/BookRepository.java
@@ -4,14 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     
 
-    Optional<Object> findByTitle(String title);
+  //  Optional<Object> findByTitle(String title);
 
-    Optional<Object> findByAuthor(String author);
+   // Optional<Object> findByAuthor(String author);
     @Query("SELECT b FROM Book b WHERE " +
             "b.title LIKE CONCAT('%',:query,'%')")
     List<Book> searchBooks(String query);
